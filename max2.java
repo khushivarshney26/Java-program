6c60c7b1ab4780c02cdc4b58b42172c2be292ef5
khushivarshney26 <141416721+khushivarshney26@users.noreply.github.com>
2023-09-18 09:33:27 +0530

    Add files via upload

---
+++ max2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class max2{
+    public static void main(String[] args){
+        int a,b;
+        Scanner r=new Scanner(System.in);
+        System.out.println("Enter the value of a:");
+        a=r.nextInt();
+        System.out.println("Enter the value of b:");
+        b=r.nextInt();
+        if(a>b){
+            System.out.println("a is maximum ");
+        }
+        else{
+            System.out.println("b is maximum " );
+        }
+    }
+}
+//Name- Khushi Varshney
+//University roll no.-2215000913
+//Sec-G
