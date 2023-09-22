a609c7a5683cd4a7ebfb610a9f7efbd6ff54adb1
khushivarshney26 <141416721+khushivarshney26@users.noreply.github.com>
2023-09-22 11:56:18 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,21 @@
+//5.Write a   Java   program to check whether a number is even or odd.
+import java.util.Scanner;
+public class evenodd {
+    public static void main(String[] args) {
+        int a;
+        System.out.println("enter the number:");
+        Scanner n=new Scanner(System.in);
+        a=n.nextInt();
+        if(a%2==0){
+            System.out.println("even number");
+        }
+        else{
+            System.out.println("odd number");
+        }
+    }
+}
+//Name-Khushi Varshney
+//Sec-30
+//Roll No-30
+//University Roll No-2215000913
+
